50d6d0961b54f129e383d40b00d76651ddb0918a
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-07-21 10:48:47 +0530

    Removed comments

--- init.el
+++ init.el
@@ -65,10 +65,6 @@
 ;; Custom Font - Monaco
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(default ((t (:height 130 :family "Monaco"))))
  '(magit-item-highlight ((t nil)) t))
 
