3c5d02c996556b8d66d59aa9d7378381aee85bf6
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-07-24 08:53:45 +0530

    Added nrepl ac related elisp

--- init.el
+++ init.el
@@ -15,8 +15,9 @@
                       ;; clojure
                       clojure-mode
                       clojure-test-mode
-
                       clojurescript-mode
+                      nrepl
+                      ac-nrepl
 
                       ;; ruby
                       rinari
@@ -249,3 +250,26 @@
 (global-set-key (kbd "C-<f3>") (rings-generate-setter 3))
 (global-set-key (kbd "<f4>")   (rings-generate-cycler 4))
 (global-set-key (kbd "C-<f4>") (rings-generate-setter 4))
+
+;; autocomplete
+
+(global-auto-complete-mode t)
+
+;; nrepl autocomplete
+
+(require 'ac-nrepl)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'nrepl-mode))
+
+;; autocomplete using TAB
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+;; ac-nrepl popup doc
+(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
