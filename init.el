b990453e06663d39f82ecad8ef4ee409398e9f33
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-08-18 00:55:35 +0530

    Don't use customize to set font, use 'set-face-attributes'

--- init.el
+++ init.el
@@ -83,14 +83,7 @@
 (mapcar 'load (directory-files hooks-dir t "\\.el\\'"))
 
 ;; Custom Font - Monaco
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :family "Monaco"))))
- '(magit-item-highlight ((t nil))))
+(set-face-attribute 'default nil :height 140 :family "monaco")
 
 ;; Load molokai theme
 (load-theme 'molokai t)
