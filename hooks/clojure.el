33b74e4aba9a4ed078ef9a4a955b095e64c6a2af
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-10-28 22:34:14 +0530

    nrepl -> cider

--- hooks/clojure.el
+++ hooks/clojure.el
@@ -1,2 +1,15 @@
 (add-hook 'clojure-mode-hook
           (lambda () (paredit-mode +1)))
+
+;; cider mode
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(setq nrepl-hide-special-buffers t)
+(setq cider-repl-pop-to-buffer-on-connect nil)
+(setq cider-popup-stacktraces nil)
+(setq cider-repl-popup-stacktraces t)
+(setq cider-auto-select-error-buffer t)
+(setq nrepl-buffer-name-show-port t)
+(setq cider-repl-display-in-current-window t)
+(add-hook 'cider-repl-mode-hook 'subword-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
