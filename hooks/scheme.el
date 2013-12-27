d51253b55ddcda83d788ffcdd4156046f4664f35
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-12-27 23:07:14 +0530

    Scheme configuration tweaked

--- hooks/scheme.el
+++ hooks/scheme.el
@@ -1,5 +1,12 @@
+(setq scheme-program-name 'guile)
 (add-hook 'scheme-mode-hook
           (lambda () (paredit-mode +1)))
 
 (add-hook 'inferior-scheme-mode-hook
           (lambda () (paredit-mode +1)))
+
+(defun scheme-mode-quack-hook ()
+  (require 'quack)
+  (setq quack-fontify-style 'emacs))
+(add-hook 'scheme-mode-hook 'scheme-mode-quack-hook)
+(add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
