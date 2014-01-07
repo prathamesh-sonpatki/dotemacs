4955f17eda9d9fdd257724220987ce44b79faba8
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-01-07 09:09:29 +0530

    Added forth mode

---
+++ hooks/forth.el
@@ -0,0 +1,3 @@
+(autoload 'forth-mode "gforth.el")
+(autoload 'forth-block-mode "gforth.el")
+(add-to-list 'auto-mode-alist '("\\.fs$" . forth-mode))
--- init.el
+++ init.el
@@ -13,6 +13,7 @@
 (setq load-path (cons "~/.emacs.d" load-path))
 (setq root-dir "~/.emacs.d")
 (setq configs-dir (concat root-dir "/configs"))
+(setq pkgs-dir (concat root-dir "/pkgs"))
 (setq hooks-dir (concat root-dir "/hooks"))
 (setq logs-dir (concat root-dir "/logs"))
 
@@ -23,6 +24,9 @@
 (require 'pps-multi-term)
 (require 'pps-settings)
 (require 'pps-keybindings)
+
+;; load the packages
+(mapcar 'load (directory-files pkgs-dir t "\\.el\\'"))
 
 ;; load the hooks
 (mapcar 'load (directory-files hooks-dir t "\\.el\\'"))
