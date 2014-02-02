6d5e78c0140fc3cde313516dda1bdb706e3c09f9
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-02-02 20:26:27 +0530

    Hack to run web mode removed
    - Problem with esk is fixed in web mode upstream, so no need to disable
      it now
    - Fixed issue with web-mode-engines-alist for erb templates

--- hooks/web.el
+++ hooks/web.el
@@ -1,13 +1,18 @@
+;;; Commentary:
+
+;;; Hooks for web mode
+
+;;; Code:
+
 (require 'web-mode)
+
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))      ;; erb
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode)) ;; mustache
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))    ;; plain html
+
 (set-face-attribute 'web-mode-html-tag-face nil :foreground "Orange")
 (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "Purple")
 (set-face-attribute 'web-mode-doctype-face nil :foreground "Blue")
 
-(setq web-mode-engines-alist '(("ruby"    . "\\.erb\\'")))
+(setq web-mode-engines-alist '(("erb" . "\\.erb\\'")))
 
-(remove-hook 'prog-mode-hook 'esk-pretty-lambdas)
-(remove-hook 'prog-mode-hook 'esk-add-watchwords)
-(remove-hook 'prog-mode-hook 'idle-highlight-mode)
+;;; web.el ends here
