7a5893496dfd66fc57635beff7b6c245e53dbeee
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-08-21 08:09:24 -0500

    Update CSS hooks

--- hooks/css.el
+++ hooks/css.el
@@ -10,3 +10,13 @@
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.scss$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.sass$" . css-mode))
+
+;;; Don't compile on save
+(setq-default scss-compile-at-save nil)
+
+;;; Use eldoc for syntax hints
+(autoload 'turn-on-css-eldoc "css-eldoc")
+(add-hook 'css-mode-hook 'turn-on-css-eldoc)
+
+(dolist (hook '(css-mode-hook html-mode-hook sass-mode-hook))
+  (add-hook hook 'rainbow-mode))
