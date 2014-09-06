61038a5b3214cc0748b97a0c61c6357ffb6376ec
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-09-06 17:51:08 +0530

    Added sane defaults
    - Based on https://github.com/magnars/.emacs.d/blob/master/sane-defaults.el

--- configs/pps-settings.el
+++ configs/pps-settings.el
@@ -77,4 +77,28 @@
 ;; Scratch buffer configuration
 ;;setq(setq initial-major-mode 'ruby-mode)
 (setq initial-scratch-message nil)
+
+;; Allow pasting selection outside of Emacs
+(setq x-select-enable-clipboard t)
+
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
+;; Show keystrokes in progress
+(setq echo-keystrokes 0.1)
+
+;; Answering just 'y' or 'n' will do
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; UTF-8 please
+(setq locale-coding-system 'utf-8) ; pretty
+(set-terminal-coding-system 'utf-8) ; pretty
+(set-keyboard-coding-system 'utf-8) ; pretty
+(set-selection-coding-system 'utf-8) ; please
+(prefer-coding-system 'utf-8) ; with sugar on top
+
 ;;; pps-settings ends here
