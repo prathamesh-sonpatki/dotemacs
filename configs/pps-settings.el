2b8817422df94eea094b4db14632eb432d53ff6a
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-12-02 14:46:18 +0530

    zenburn

--- configs/pps-settings.el
+++ configs/pps-settings.el
@@ -6,11 +6,11 @@
 ;;; Code:
 
 ;; Theme
-(load-theme 'mbo70s t)
+(load-theme 'zenburn t)
 
 ;; Custom Font
 (set-frame-font "Source Code Pro")
-(set-face-attribute 'default nil :font "Source Code Pro" :height 210)
+(set-face-attribute 'default nil :font "Source Code Pro" :height 150)
 (set-face-font 'default "Source Code Pro")
 
 ;; Start in full screen mode
