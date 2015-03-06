f89386c86248e8afe5741793fb4c6325afc31519
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-03-06 15:43:18 +0530

    Updated colors for web mode

--- hooks/web.el
+++ hooks/web.el
@@ -10,9 +10,10 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))    ;; plain html
 (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))     ;; jsx
 
-(set-face-attribute 'web-mode-html-tag-face nil :foreground "Orange")
-(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "Purple")
-(set-face-attribute 'web-mode-doctype-face nil :foreground "Blue")
+
+(set-face-attribute 'web-mode-html-tag-face nil :foreground "Red")
+(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "Blue")
+(set-face-attribute 'web-mode-doctype-face nil :foreground "Green")
 
 (setq web-mode-engines-alist '(("erb" . "\\.erb\\'")))
 
