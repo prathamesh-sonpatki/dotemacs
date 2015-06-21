94b411273e6088543ce239c479c3df72f0f6b6a8
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-06-21 22:56:59 +0530

    Added hooks for sending line, region and buffer to lua process specific to lua mode.
     Used https://www.masteringemacs.org/article/mastering-key-bindings-emacs as reference.

--- hooks/lua.el
+++ hooks/lua.el
@@ -2,3 +2,8 @@
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+;;; Custom keybindings for evaluating code
+(define-key lua-mode-map (kbd "C-x C-e") 'lua-send-current-line)
+(define-key lua-mode-map (kbd "C-x C-r") 'lua-send-region)
+(define-key lua-mode-map (kbd "C-x C-x") 'lua-send-buffer)
