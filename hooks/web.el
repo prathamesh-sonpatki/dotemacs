a1e1619146cac242e16443625729cbef663f40f4
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-07-12 13:34:32 +0530

    Configured web mode for .blade.php and .es6 templates

--- hooks/web.el
+++ hooks/web.el
@@ -8,15 +8,23 @@
 
 (require 'web-mode)
 
-(add-to-list 'auto-mode-alist '("\\.erb\\'"    . web-mode))   ;; ERB
-(add-to-list 'auto-mode-alist '("\\.html?\\'"  . web-mode))   ;; plain html
-(add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))   ;; JS + JSX
-(add-to-list 'auto-mode-alist '("\\.es6\\'"    . web-mode))   ;; ES6
-(add-to-list 'auto-mode-alist '("\\.css\\'"    . web-mode))   ;; CSS
-(add-to-list 'auto-mode-alist '("\\.scss\\'"   . web-mode))   ;; SCSS
+(add-to-list 'auto-mode-alist '("\\.erb\\'"    . web-mode))       ;; ERB
+(add-to-list 'auto-mode-alist '("\\.html?\\'"  . web-mode))       ;; Plain HTML
+(add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))       ;; JS + JSX
+(add-to-list 'auto-mode-alist '("\\.es6\\'"    . web-mode))       ;; ES6
+(add-to-list 'auto-mode-alist '("\\.css\\'"    . web-mode))       ;; CSS
+(add-to-list 'auto-mode-alist '("\\.scss\\'"   . web-mode))       ;; SCSS
+(add-to-list 'auto-mode-alist '("\\.php\\'"   . web-mode))        ;; PHP
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))  ;; Blade template
 
 (setq web-mode-content-types-alist
-  '(("jsx" . "\\.js[x]?\\'")))
+   '(("jsx" . "\\.js[x]?\\'")))
+
+(setq web-mode-content-types-alist
+  '(("javascript" . "\\.es6?\\'")))
+
+(setq web-mode-engines-alist
+      '(("blade"  . "\\.blade\\.")))
 
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
