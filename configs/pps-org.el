1e7fe47d7eebd665474149f103313584151f4040
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-07-18 22:39:38 +0530

    Removed unused org settings

--- configs/pps-org.el
+++ configs/pps-org.el
@@ -4,55 +4,6 @@
 
 (define-key mode-specific-map [?a] 'org-agenda)
 
-(eval-after-load "org"
-  '(progn
-     (define-prefix-command 'org-todo-state-map)
-
-     (define-key org-mode-map "\C-cx" 'org-todo-state-map)
-
-     (define-key org-todo-state-map "x"
-       #'(lambda nil (interactive) (org-todo "CANCELLED")))
-     (define-key org-todo-state-map "d"
-       #'(lambda nil (interactive) (org-todo "DONE")))
-     (define-key org-todo-state-map "f"
-       #'(lambda nil (interactive) (org-todo "DEFERRED")))
-     (define-key org-todo-state-map "l"
-       #'(lambda nil (interactive) (org-todo "DELEGATED")))
-     (define-key org-todo-state-map "s"
-       #'(lambda nil (interactive) (org-todo "STARTED")))
-     (define-key org-todo-state-map "w"
-       #'(lambda nil (interactive) (org-todo "WAITING")))))
-
-(custom-set-variables
- '(org-agenda-ndays 7)
- '(org-deadline-warning-days 14)
- '(org-agenda-show-all-dates t)
- '(org-agenda-skip-deadline-if-done t)
- '(org-agenda-skip-scheduled-if-done t)
- '(org-agenda-start-on-weekday nil)
- '(org-reverse-note-order t)
- '(org-fast-tag-selection-single-key (quote expert))
- '(org-agenda-custom-commands
-   (quote (("d" todo "DELEGATED" nil)
-	   ("c" todo "DONE|DEFERRED|CANCELLED" nil)
-	   ("w" todo "WAITING" nil)
-	   ("W" agenda "" ((org-agenda-ndays 21)))
-	   ("A" agenda ""
-	    ((org-agenda-skip-function
-	      (lambda nil
-		(org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]")))
-	     (org-agenda-ndays 1)
-	     (org-agenda-overriding-header "Today's Priority #A tasks: ")))
-	   ("u" alltodo ""
-	    ((org-agenda-skip-function
-	      (lambda nil
-		(org-agenda-skip-entry-if (quote scheduled) (quote deadline)
-					  (quote regexp) "\n]+>")))
-	     (org-agenda-overriding-header "Unscheduled TODO entries: "))))))
- '(org-remember-store-without-prompt t)
- '(remember-annotation-functions (quote (org-remember-annotation)))
- '(remember-handler-functions (quote (org-remember-handler))))
-
 (setq org-log-done 'time)
 (setq org-deadline-warning-days 2)
 (setq org-clock-persist 'history)
