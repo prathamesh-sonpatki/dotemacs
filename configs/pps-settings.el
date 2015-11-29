19a8c5b6eaf7890e51c25371ba386505420e7f01
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-11-29 20:55:33 +0530

    Ignore .cask directory too in projectile

--- configs/pps-settings.el
+++ configs/pps-settings.el
@@ -37,6 +37,7 @@
 (add-to-list 'projectile-globally-ignored-directories "elpa")
 (add-to-list 'projectile-globally-ignored-directories ".cache")
 (add-to-list 'projectile-globally-ignored-directories "node_modules")
+(add-to-list 'projectile-globally-ignored-directories ".cask")
 (setq projectile-use-git-grep t)
 
 ;; pallet
