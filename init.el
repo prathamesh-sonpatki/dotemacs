17d83034dc11833eea607878cbfc27efb6b4fbee
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-01-12 10:35:49 +0530

    Hide show setup

--- init.el
+++ init.el
@@ -53,4 +53,26 @@
                     :height 170
                     :weight 'normal)
 
+(defun toggle-selective-display (column)
+  (interactive "P")
+  (set-selective-display
+   (or column
+       (unless selective-display
+         (1+ (current-column))))))
+
+(defun toggle-hiding (column)
+  (interactive "P")
+  (if hs-minor-mode
+      (if (condition-case nil
+              (hs-toggle-hiding)
+            (error t))
+          (hs-show-all))
+    (toggle-selective-display column)))
+
+(load-library "hideshow")
+(global-set-key (kbd "C-+") 'toggle-hiding)
+(global-set-key (kbd "C-\\") 'toggle-selective-display)
+(add-hook 'c-mode-common-hook   'hs-minor-mode)
+(add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
+(add-hook 'enh-ruby-mode-hook         'hs-minor-mode)
 ;;; init.el ends here
