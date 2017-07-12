c5b77d611a7dbdb502e47eb0e04adbd7d7633d43
Prathamesh Sonpatki <csonpatki@gmail.com>
2017-07-12 11:35:38 +0530

    Elm support

--- configs/pps-settings.el
+++ configs/pps-settings.el
@@ -260,5 +260,17 @@
 (setenv "PATH" (concat "/usr/local/smlnj/bin:" (getenv "PATH")))
 (setq exec-path (cons "/usr/local/smlnj/bin"  exec-path))
 
+;; ELM
+(require 'elm-mode)
+(setq elm-format-on-save t)
+(setq elm-tags-on-save t)
+(add-hook 'flycheck-mode-hook 'flycheck-elm-setup)
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'company-elm))
+(add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
+(add-hook 'elm-mode-hook
+          (lambda ()
+            (setq company-backends '(company-elm))))
+
 (provide 'pps-settings)
 ;;; pps-settings ends here
