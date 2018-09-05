8a30ef3ba9cabd1b4d4b7471c6ff6dd2257361d0
Prathamesh Sonpatki <csonpatki@gmail.com>
2018-09-05 13:54:12 +0530

    Inconsolata

--- init.el
+++ init.el
@@ -54,9 +54,9 @@
 
 ;; Font
 (set-face-attribute 'default nil
-                    :font "Source Code Pro"
+                    :font "Inconsolata"
                     :height 220
-                    :weight 'medium)
+                    :weight 'bold)
 
 (defun toggle-selective-display (column)
   (interactive "P")
