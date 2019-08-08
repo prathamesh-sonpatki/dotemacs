dc8f5a5643f4333ed0a62f0805b021fae38767c1
Prathamesh Sonpatki <csonpatki@gmail.com>
2019-08-08 12:19:17 +0530

    Enabled auto-complete

--- hooks/enh-ruby.el
+++ hooks/enh-ruby.el
@@ -44,6 +44,11 @@
 (custom-set-variables
  '(robe-completing-read-func 'helm-robe-completing-read))
 (require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+(setq ac-ignore-case nil)
+(add-to-list 'ac-modes 'enh-ruby-mode)
+(add-to-list 'ac-modes 'web-mode)
 (add-hook 'robe-mode-hook 'ac-robe-setup)
 
 
